fbe6e1ee899c0c0e1330428be6c9958b55298ef8
park <tlsrja10@naver.com>
2019-03-26 16:32:48 +0900

    MemberVO 생성

---
+++ gitTest2/src/gittest2/MemberVO.java
@@ -0,0 +1,18 @@
+package gittest2;
+
+public class MemberVO {
+	private String name;
+	private String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
